e56b54088b55086ea2ea2ddcdefd508cd85a77e9
Taiphong2006 <tphphong1234@gmail.com>
2024-12-11 15:26:18 +0700

    A-enter-print

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to enter-print
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/enter_print/core.clj
@@ -0,0 +1,54 @@
+(ns enter-print.core)
+
+(defn nhap-so
+  []
+  (loop [n (Integer/parseInt (read-line))
+         s []]
+    (if (= n 0)
+      s
+      (recur (dec n) (conj s (Integer/parseInt (read-line)))))))
+
+(nhap-so)
+(apply max (nhap-so))
+(defn max
+  []
+  (apply max (nhap-so)))
+(max)
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
