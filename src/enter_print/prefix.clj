815339b9723f1ec34815720993818d9554d7079c
Taiphong2006 <tphphong1234@gmail.com>
2024-12-14 16:01:31 +0700

    Ban moi bai bbsort

--- src/enter_print/prefix.clj
+++ src/enter_print/prefix.clj
@@ -17,7 +17,6 @@
         ""
         (str prefix (longest-prefix (map rest s)))))))
 
-(longest-prefix ["ab" "ab"])
 
 
 
@@ -38,18 +37,4 @@
 
 
 
-
-(deftest test-longest-prefix
-  (testing "Common prefix exists"
-    (is (= "fl" (longest-prefix ["flower" "flow" "flight"])))
-    (is (= "test" (longest-prefix ["testcase" "testdata" "testing"]))))
-  (testing "No common prefix"
-    (is (= "" (longest-prefix ["dog" "cat" "fish"])))
-    (is (= "" (longest-prefix ["apple" "banana" "cherry"]))))
-  (testing "Edge cases"
-    (is (= "" (longest-prefix []))) ; Empty list of strings
-    (is (= "" (longest-prefix [""]))) ; Single empty string
-    (is (= "a" (longest-prefix ["a"]))) ; Single character string
-    (is (= "hello" (longest-prefix ["hello" "hello" "hello"])))))
- ; Identical strings
 
